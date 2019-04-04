42b65611ba4d898f20e1e4f249925b54620a67c9
Mark Hegedus <hmark33@gmail.com>
2019-04-04 15:40:05 +0200

    feat: add swap.ts

--- week-01/day-04/swap.ts
+++ week-01/day-04/swap.ts
@@ -6,7 +6,10 @@ let b: number = 526;
 console.log('"a" is ' + a + '.');
 console.log('"b" is ' + b + '.');
 
-[a,b] = [b,a];
+let c: number = a + b;
+
+a = c - a;
+b = c - a;
 
 console.log('"a" is now ' + a + '.');
 console.log('"b" is now ' + b + '.');
