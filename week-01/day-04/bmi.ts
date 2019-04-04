0eab3c918bc37051f119d042d50efadfc646428c
Mark Hegedus <hmark33@gmail.com>
2019-04-04 15:48:13 +0200

    feat: add bmi.ts

---
+++ week-01/day-04/bmi.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let massInKg = 81.2;
+let heightInM = 1.78;
+
+let BMI = massInKg / (heightInM * heightInM);
+
+console.log('If mass is ' + massInKg + ' kg, and height is ' + heightInM + ' m, the BMI is ' + BMI + ' kg/m2.');
