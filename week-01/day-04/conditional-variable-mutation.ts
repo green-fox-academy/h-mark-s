c6ddaf768acdfa122e5422bf96fffc1f680045d6
Mark Hegedus <hmark33@gmail.com>
2019-04-05 09:58:45 +0200

    feat: add conditional-variable-mutation.ts

--- week-01/day-04/conditional-variable-mutation.ts
+++ week-01/day-04/conditional-variable-mutation.ts
@@ -26,25 +26,26 @@ let c: number = 123;
 let credits: number = 100;
 let isBonus: boolean = false;
 
-if (credits >= 50 && isBonus === false){
+if (credits >= 50 && !isBonus){
     c = c - 2;
-} else if (credits < 50 && isBonus === false){
+} else if (credits < 50 && !isBonus){
     c = c - 1;
+} else if (isBonus){
+    c = c;
 }
 
-// if is_bonus is true c should remain the same
-
-console.log(c);
+console.log(c)
 
 let d: number =  8;
 let time: number = 120;
 let out3: string = '';
 
-// if d is dividable by 4
-// and time is not more than 200
-// set out3 to "check"
-// if time is more than 200
-// set out3 to "Time out"
-// otherwise set out3 to "Run Forest Run!"
+if (d % 4 === 0 && time <= 200){
+    out3 = 'check';
+} else if (time > 200){
+    out3 = 'Time out';
+} else {
+    out3 = "Run Forrest Run!"
+}
 
 console.log(out3);
