b286457e0cae263ee8c4b11fdacd08c3b0723379
Mark Hegedus <hmark33@gmail.com>
2019-04-05 11:26:39 +0200

    feat: add draw-triangle.ts

---
+++ week-01/day-05/draw-triangle.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let lineCount: number = 4;
+let triangle: string = '';
+let triangleComponent: string = '*';
+let a: number = 1;
+
+for (let i: number = 1; i <=lineCount; i++) {
+    triangle = triangle + triangleComponent;
+    console.log(triangle);
+}
