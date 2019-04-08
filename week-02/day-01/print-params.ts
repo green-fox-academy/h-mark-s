1709b5eb5be04eade2a9daab2b19d77e24e67d7b
Mark Hegedus <hmark33@gmail.com>
2019-04-08 15:34:34 +0200

    fix(print-params.ts): fixed function again

--- week-02/day-01/print-params.ts
+++ week-02/day-01/print-params.ts
@@ -5,7 +5,3 @@ function printParams(...dolgok: any[]): void {
 }
 
 printParams('ember', 8, 1);
-
-// -  Create a function called `printParams`
-//    which logs to the console the input parameters
-//    (can have multiple number of arguments)
