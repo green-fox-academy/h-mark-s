649469ad0a561ea9ef88cd63a2d10276bde22e11
Mark Hegedus <hmark33@gmail.com>
2019-04-08 14:25:03 +0200

    feat(sum.ts): add sum exercise

---
+++ week-02/day-01/sum.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let addTill: number = 5;
+let sum: number = 0;
+
+for (let i = 0; i <= addTill; i++) {
+    sum = sum + i;
+}
+
+console.log(sum);
+
+// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
