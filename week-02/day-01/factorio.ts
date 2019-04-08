f466586ce867d707e1fda449c18a8a0eb7aa39f1
Mark Hegedus <hmark33@gmail.com>
2019-04-08 15:44:36 +0200

    fix(factorio.ts): fixed function to return correct answer to "0"

--- week-02/day-01/factorio.ts
+++ week-02/day-01/factorio.ts
@@ -5,6 +5,8 @@ let baseOfFactorial: number = 4;
 function factorio(base: number): any {
     if (baseOfFactorial < 0) {
         return 'The number mustn\'t be negative!';
+    } else if (baseOfFactorial === 0) {
+        return 1;
     } else {
     let sum: number = 1;
     for (let i = 1; i <= baseOfFactorial; i++) {
