8dfd329ca1d0b42d0e10e3c9d251ab8ef9f74967
Mark Hegedus <hmark33@gmail.com>
2019-04-08 14:33:49 +0200

    fix(sum.ts): add actual function

--- week-02/day-01/sum.ts
+++ week-02/day-01/sum.ts
@@ -1,12 +1,13 @@
 'use strict';
 
 let addTill: number = 5;
-let sum: number = 0;
 
-for (let i = 0; i <= addTill; i++) {
-    sum = sum + i;
+function sum (number: number): number {
+    let sum: number = 0;
+    for (let i = 1; i <= addTill; i++) {
+        sum = sum + i;
+    }
+    return sum;
 }
 
-console.log(sum);
-
-// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+console.log(sum(addTill));
