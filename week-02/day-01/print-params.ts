4a5f206d65a0711a4a90d51d3a50d5c8c870611b
Mark Hegedus <hmark33@gmail.com>
2019-04-08 15:15:18 +0200

    feat(print-params.ts): add print-params exercise

---
+++ week-02/day-01/print-params.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let input: any[] = ['ember', 1, 8];
+
+function printParams(): any {
+    console.log(input); 
+}
+
+printParams();
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
