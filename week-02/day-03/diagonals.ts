ef1ce99c027448b7cd6269b37fa15654417f416b
Mark Hegedus <hmark33@gmail.com>
2019-04-10 11:05:21 +0200

    feat(diagonals.ts): add diagonals exercise

---
+++ week-02/day-03/diagonals.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx:any = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw the canvas' diagonals.
+// If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+ctx.beginPath();
+ctx.strokeStyle = 'green';
+ctx.moveTo(0, 0);
+ctx.lineTo(600, 600);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.moveTo(600, 0);
+ctx.lineTo(0, 600);
+ctx.stroke();
