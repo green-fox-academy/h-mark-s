945c39325f263760669e7f1fc1b9f9c15a38288e
Mark Hegedus <hmark33@gmail.com>
2019-04-10 12:23:38 +0200

    feat(four-rectangles-ts.ts): add four rectangles exercise

---
+++ week-02/day-03/four-rectangles-ts.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx: any = canvas.getContext('2d');
+
+// Draw four different size and color rectangles.
+// Avoid code duplication.
+
+ctx.beginPath();
+ctx.fillStyle = 'blue';
+ctx.fillRect(50, 50, 100, 200);
+
+ctx.beginPath();
+ctx.fillStyle = 'green';
+ctx.fillRect(550, 50, 40, 90);
+
+ctx.beginPath();
+ctx.fillStyle = 'yellow';
+ctx.fillRect(300, 450, 92, 123);
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.fillRect(200, 380, 280, 50);
