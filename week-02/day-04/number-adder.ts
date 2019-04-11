005fc90c1cb831a3b68325b49a589d5b476b93e0
Mark Hegedus <hmark33@gmail.com>
2019-04-11 10:39:46 +0200

    feat(number-adder.ts): add number adder exercise

---
+++ week-02/day-04/number-adder.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+function numberAdder(n: number): number {
+  if (n === 1) {
+    return 1;
+  } else if (n < 1) {
+    return 0;
+  } else {
+  return n + numberAdder(n-1);
+  }
+}
+
+console.log(numberAdder(2));
