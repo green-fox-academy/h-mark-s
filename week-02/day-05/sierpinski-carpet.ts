e73a5060c36584c5c98f5b8724a67947da857826
Mark Hegedus <hmark33@gmail.com>
2019-04-12 14:27:42 +0200

    feat(sierpinski-carpet.ts): add sierpinski carpet exercise

---
+++ week-02/day-05/sierpinski-carpet.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx: any = canvas.getContext('2d');
+
+const rectLength: number = canvas.width / 3;
+
+function sierpinskiCarpet(startX: number, startY: number, length: number, depth: number): void {
+  if (length < canvas.width / depth) {
+    return;
+  } else {
+    ctx.beginPath();
+    ctx.fillStyle = 'black';
+    ctx.fillRect(startX + length, startY + length, length, length);
+  
+    sierpinskiCarpet(startX, startY, length / 3, depth)
+    sierpinskiCarpet(startX + length, startY, length / 3, depth)
+    sierpinskiCarpet(startX + length * 2, startY, length / 3, depth)
+    sierpinskiCarpet(startX, startY + length, length / 3, depth)
+    sierpinskiCarpet(startX, startY + length * 2, length / 3, depth)
+    sierpinskiCarpet(startX + length * 2, startY + length, length / 3, depth)
+    sierpinskiCarpet(startX + length, startY + length * 2, length / 3, depth)
+    sierpinskiCarpet(startX + length * 2, startY + length * 2, length / 3, depth)
+    
+  }
+}
+
+sierpinskiCarpet(0, 0, rectLength, 1000);
