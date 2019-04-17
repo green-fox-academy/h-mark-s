c6df83635feb0ac97d909a6a18e5cb7fc6e039c6
Mark Hegedus <hmark33@gmail.com>
2019-04-17 12:04:51 +0200

    feat(blog-post.ts): add blog post exercise

---
+++ week-03/day-03/blog-post.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+class BlogPost{
+  authorName: string;
+  title: string;
+  text: string;
+  publicationDate: string;
+
+  constructor(authorName: string, title: string, text: string, publicationDate: string){
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+
+  showMyBlogPost(): void {
+    console.log(`"${this.title}" by ${this.authorName} posted at ${this.publicationDate}:\n\t${this.text}`)
+  }
+}
+
+const firstPost: BlogPost = new BlogPost('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet.', '2000.05.04.');
+const secondPost: BlogPost = new BlogPost('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10.');
+const thirdPost: BlogPost = new BlogPost('William Turton', 'One Engineer Is Trying to Get IBM to Reckon With Trump', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.', '2017.03.28.');
+
+firstPost.showMyBlogPost();
+secondPost.showMyBlogPost();
+thirdPost.showMyBlogPost();
