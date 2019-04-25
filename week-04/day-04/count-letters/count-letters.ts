c1778c801a4dfc1c8d52292891902c44154d664f
Mark Hegedus <hmark33@gmail.com>
2019-04-25 14:46:58 +0200

    feat(count-letters.ts): add countLetters function

--- week-04/day-04/count-letters/count-letters.ts
+++ week-04/day-04/count-letters/count-letters.ts
@@ -1,7 +1,24 @@
-'use strict';
+"use strict";
 
-function countLetters (): void {
-
+function countLetters(stringToCheck: string): object {
+  let objectOfLetters: {
+    [key: string]: number;
+  } = {};
+  for (let i: number = 0; i < stringToCheck.length; i++) {
+    let letterCount: number = 0;
+    let letterToCheck: string = stringToCheck.charAt(i);
+    for (let j: number = 0; j < stringToCheck.length; j++) {
+      if (stringToCheck.charAt(j) === letterToCheck) {
+        letterCount++;
+      }
+    }
+    if (!objectOfLetters.hasOwnProperty(letterToCheck)) {
+      objectOfLetters[letterToCheck] = letterCount;
+    }
+  }
+  return objectOfLetters;
 }
 
-export { countLetters }
+console.log(countLetters("mfdsafkldsammmmdfsaaaa"));
+
+export { countLetters };
