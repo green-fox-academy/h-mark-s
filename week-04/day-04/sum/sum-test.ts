1c0f98d3a525728af8ceca373fb5b58583726122
Mark Hegedus <hmark33@gmail.com>
2019-04-25 12:11:53 +0200

    feat(sum-test.ts): add different tests

--- week-04/day-04/sum/sum-test.ts
+++ week-04/day-04/sum/sum-test.ts
@@ -15,3 +15,25 @@ test('return sum of numbers', function(t: any) {
   t.end();
 
 });
+
+test('sum of an empty array', function(t:any) {
+  const numbers1: Numbers = new Numbers();
+
+  const actual = numbers1.sum([]);
+  const expected: number = 0;
+
+  t.equal(actual, expected);
+  t.end();
+
+});
+
+test('sum of one element', function (t:any) {
+  const numbers1: Numbers = new Numbers();
+
+  const actual = numbers1.sum([1]);
+  const expected: number = 1;
+
+  t.equal(actual, expected);
+  t.end();
+
+})
