1df2531f3fdf2f158c058fe0a7992c7d09eb1b5a
Mark Hegedus <hmark33@gmail.com>
2019-04-25 13:11:01 +0200

    chore(count-letters): create files for exercise

---
+++ week-04/day-04/count-letters/count-letters-test.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+import { countLetters } from './count-letters';
+
+declare function require(path: string): any;
+var test = require('tape').test;
---
+++ week-04/day-04/count-letters/count-letters.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+function countLetters (): void {
+
+}
+
+export { countLetters }
