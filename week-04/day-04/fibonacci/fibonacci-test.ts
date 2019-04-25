91b82789afb3f38953a36453915d28a44caff307
Mark Hegedus <hmark33@gmail.com>
2019-04-25 16:00:54 +0200

    feat(fibonacci-test.ts): add fibonacci test

--- week-04/day-04/fibonacci/fibonacci-test.ts
+++ week-04/day-04/fibonacci/fibonacci-test.ts
@@ -5,3 +5,11 @@ import { fibonacci } from './fibonacci';
 declare function require(path: string): any;
 var test = require('tape').test;
 
+test('fibonacci function', function (t: any) {
+  
+  const actual: number = fibonacci(10);
+  const expected: number = 55;
+
+  t.equal(actual, expected);
+  t.end();
+});
