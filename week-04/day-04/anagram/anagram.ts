9d64e4e7ca153ca4cc60cb75b8a00cd809b8f88d
Mark Hegedus <hmark33@gmail.com>
2019-04-25 12:24:50 +0200

    feat(anagram.ts): create anagram function

---
+++ week-04/day-04/anagram/anagram.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+function anagram(a: string, b: string): boolean {
+  let x: string = a.split('').sort().join('');
+  let y: string = b.split('').sort().join('');
+  return x === y;
+}
+
+console.log(anagram('mark', 'kram'))
