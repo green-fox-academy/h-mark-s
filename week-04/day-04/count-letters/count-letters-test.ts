f8b08d9978302aedf1fcbfbb790fb712da965050
Mark Hegedus <hmark33@gmail.com>
2019-04-25 14:53:24 +0200

    feat(count-letters-test.ts): add test function

--- week-04/day-04/count-letters/count-letters-test.ts
+++ week-04/day-04/count-letters/count-letters-test.ts
@@ -4,3 +4,18 @@ import { countLetters } from './count-letters';
 
 declare function require(path: string): any;
 var test = require('tape').test;
+
+
+test('counting letters in string', function (t: any) {
+
+  const actual = countLetters('markrk');
+  const expected: object = {
+    m: 1,
+    a: 1,
+    r: 2,
+    k: 2
+  };
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
