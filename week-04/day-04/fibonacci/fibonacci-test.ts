0ddbd7107596bf0fdd8de8089e72bde6cfdf8435
Mark Hegedus <hmark33@gmail.com>
2019-04-25 14:59:09 +0200

    chore(fibonacci): create files for exercise

---
+++ week-04/day-04/fibonacci/fibonacci-test.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+import { fibonacci } from './fibonacci';
+
+declare function require(path: string): any;
+var test = require('tape').test;
+
---
+++ week-04/day-04/fibonacci/fibonacci.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+function fibonacci(index: number): number {
+
+}
+
+export { fibonacci };
