2e75c5ca0dac8ffd6f8961aa634c37ef084bf0c3
Mark Hegedus <hmark33@gmail.com>
2019-04-25 15:57:55 +0200

    feat(fibonacci.ts): add fibonacci function

--- week-04/day-04/fibonacci/fibonacci.ts
+++ week-04/day-04/fibonacci/fibonacci.ts
@@ -1,7 +1,11 @@
-'use strict';
+"use strict";
 
 function fibonacci(index: number): number {
-
+  let fibonacciList: number[] = [0, 1];
+  for (let i: number = 2; i < index + 1; i++) {
+    fibonacciList[i] = fibonacciList[i - 1] + fibonacciList[i - 2];
+  }
+  return fibonacciList[index];
 }
 
 export { fibonacci };
