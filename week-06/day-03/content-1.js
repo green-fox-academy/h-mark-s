e94b79ecd78002fcbb52c29b1f6c7306074c576b
Mark Hegedus <hmark33@gmail.com>
2019-05-15 12:21:09 +0200

    feat(content-1): complete exercise

---
+++ week-06/day-03/content-1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const apple = document.querySelector(".apple");
+const balloon = document.querySelector(".balloon");
+const cat = document.querySelector(".cat");
+const animals = document.querySelector(".animals")
+
+apple.innerHTML = animals.innerHTML;
+balloon.innerHTML = animals.innerHTML;
+cat.innerHTML = animals.innerHTML;
