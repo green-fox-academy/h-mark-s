e8537e8ba8ccf3db8865429d1eb7860715c92d16
Mark Hegedus <hmark33@gmail.com>
2019-05-22 11:11:50 +0200

    chore(frontend): create basic file structure

---
+++ week-07/day-03/frontend/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 8080;
+
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
