8ffe32983157ef27b496a125824ce0677201b56a
Mark Hegedus <hmark33@gmail.com>
2019-06-06 10:06:22 +0200

    feat(questions.js): input fields clear after submit

--- week-09/day-04/quiz-app/public/questions.js
+++ week-09/day-04/quiz-app/public/questions.js
@@ -8,33 +8,36 @@ addQuestion.addEventListener('submit', (event) => {
   xhr.open('POST', 'http://localhost:5500/api/questions');
   xhr.setRequestHeader('Content-Type', 'application/json');
   let isCorrectArray = [];
-  for (let i = 0; i < document.querySelectorAll('input[type="radio"]').length; i++) {
-    if (document.querySelectorAll('input[type="radio"]')[i].checked === true) {
-      console.log(document.querySelectorAll('input[type="radio"]')[i])
+  const fields = document.querySelectorAll('input[type="text"]');
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked === true) {
       isCorrectArray.push("1");
     } else {
-      console.log(document.querySelectorAll('input[type="radio"]')[i].checked)
       isCorrectArray.push("0");
     }
   }
-  console.log(isCorrectArray);
+  xhr.onload = () => {
+    fields.forEach(field => field.value = '');
+    radioButtons.forEach(button => button.checked = false);
+  }
   xhr.send(JSON.stringify({
-    "question": document.querySelector('#question').value,
+    "question": event.target.elements.question.value,
     "answers": [
       {
-        "answer": document.querySelector('#answer-one').value,
+        "answer": event.target.elements["answer-one"].value,
         "is_correct": isCorrectArray[0],
       },
       {
-        "answer": document.querySelector('#answer-two').value,
+        "answer": event.target.elements["answer-two"].value,
         "is_correct": isCorrectArray[1],
       },
       {
-        "answer": document.querySelector('#answer-three').value,
+        "answer": event.target.elements["answer-three"].value,
         "is_correct": isCorrectArray[2],
       },
       {
-        "answer": document.querySelector('#answer-four').value,
+        "answer": event.target.elements["answer-four"].value,
         "is_correct": isCorrectArray[3],
       }
     ]
