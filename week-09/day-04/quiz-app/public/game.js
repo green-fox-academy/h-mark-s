870351d531f0e3de7d86e2ece7d512c7d3bd72ee
Mark Hegedus <hmark33@gmail.com>
2019-06-06 10:49:57 +0200

    feat(game.js): create answer click events

--- week-09/day-04/quiz-app/public/game.js
+++ week-09/day-04/quiz-app/public/game.js
@@ -5,17 +5,22 @@ const question = document.querySelector('.question');
 const answers = document.querySelectorAll('.answer');
 
 let currentScore = 0;
-score.innerText = currentScore;
 
 function newQuestion() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://localhost:5500/api/game');
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onload = data => {
+    score.innerText = currentScore;
     let response = JSON.parse(data.target.response);
     question.innerText = response.question;
     for (let i = 0; i < answers.length; i++) {
       answers[i].innerText = response.answers[i].answer;
+      if (response.answers[i]['is_correct'] === 1) {
+        answers[i].setAttribute('class', 'correct');
+      } else {
+        answers[i].setAttribute('class', 'incorrect');
+      }
     }
   }
   xhr.send();
@@ -25,8 +30,19 @@ window.onload = () => {
   newQuestion();
 }
 
-answers.forEach((answer) => {
+answers.forEach(answer => {
   answer.addEventListener('click', () => {
-    alert('feature coming soon');
+    if (answer.classList.contains('correct')) {
+      answer.setAttribute('style', 'background: green');
+      currentScore++;
+      score.innerText = currentScore;
+    } else {
+      answer.setAttribute('style', 'background: red');
+      document.querySelector('.correct').setAttribute('style', 'background: green');
+    }
+    setTimeout(() => {
+      newQuestion();
+      answers.forEach(answer => answer.setAttribute('style', ''))
+    }, 3000);
   });
 });
